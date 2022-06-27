ae4eab572b74bb7b5bcab506a141e608aead8cf6
Alina <plastovecalina@gmail.com>
2022-06-28 00:03:25 +0300

    jump

--- src/sky/sun.ts
+++ src/sky/sun.ts
@@ -1,1 +1,8 @@
-export class Sun { }
+export class Sun {
+    constructor(private x:number, private y:number, private d:number) {}
+
+    draw() {
+        fill ("orange");
+        circle(20, 20, 30);
+    }
+}
